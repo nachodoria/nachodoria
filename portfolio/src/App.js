d69fc9189494cd5288a787de45aaa7f5b25a3cd9
nachodoria <ignaciodoriaoberman@gmail.com>
2022-11-02 12:08:19 -0300

    Started coding Figma

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola Mundo</h1>
+  <>
+    <div className='main'>
+      <div className="App">
+        <h1>ignacio doria</h1>
+        <div className='nave'></div>
+      </div>
     </div>
+
+      <div className='ab-me-section'>
+          <h1>about me</h1>
+          <h2>i'm an IT Student</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+  </>
   );
 }
 
