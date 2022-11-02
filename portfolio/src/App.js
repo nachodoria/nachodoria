1712bdb3b02d60392a51b6d763fcd38bb066f6dd
nachodoria <ignaciodoriaoberman@gmail.com>
2022-11-02 10:39:25 -0300

    Starting

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Hola Mundo</h1>
     </div>
   );
 }
