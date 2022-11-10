a91281a91cb3b5b627ce6569b687821ca3ae2c3f
nachodoria <ignaciodoriaoberman@gmail.com>
2022-11-10 11:24:02 -0300

    Locomotive and Social media

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,8 +2,7 @@ import './App.css';
 import "./locomotive-scroll.css"
 import React, { useState, useEffect, useRef } from 'react';
 
-import locomotiveScroll from "locomotive-scroll";
-
+import LocomotiveScroll from 'locomotive-scroll';
 
 
 function App() {
@@ -14,11 +13,12 @@ function App() {
 
   useEffect(() => {
 
-    const scroll = new locomotiveScroll({
+    const scroll = new LocomotiveScroll({
       el: scrollRef.current,
       smooth: true,
     });
 
+    
 
   });
 
@@ -41,7 +41,7 @@ function App() {
     <>
       <div className='scroll'  ref={scrollRef}>
         <div className="main" data-scroll-container >
-          <div className="start" data-scroll-section data-scroll-section-id="section1">
+          <div className="start" data-scroll-section data-scroll-section-id="section1" id='js-target'>
             <h1
               data-scroll
               data-scroll-speed="4"
@@ -64,7 +64,7 @@ function App() {
           </div>
         </div>
 
-        <div className="ab-me-section" data-scroll-section data-scroll-section-id="section2" >
+        <div className="ab-me-section" data-scroll-section data-scroll-section-id="section2"  >
           <h1
             data-scroll
             data-scroll-direction="vectical"
@@ -74,9 +74,13 @@ function App() {
            data-scroll
            data-scroll-direction="vectical"
            data-scroll-position="top"
+           data-scroll-delay	="0.1"
 
            data-scroll-speed="1">i'm an IT Student</h2>
           <p
+          data-scroll
+          data-scroll-speed="2"
+          data-scroll-direction="vectical"
           
            >My name is Ignacio Doria, I am a high school student in Argentina. Fortunately I discovered the front-end development skills and started developing them at a young age. I started to learn these skills in 2020 and now I can tell that I know JS, React JS, CSS, HTML, Python, and some PHP for some back-end needs. I am open-minded to the process of learning.</p>
         </div>
@@ -89,22 +93,41 @@ function App() {
           <h2
             data-scroll
             data-scroll-direction="vectical"
-            data-scroll-position="bottom"
- 
-            data-scroll-speed="2"
+            data-scroll-position="bottom" 
+             data-scroll-delay	="0.1"
+            data-scroll-speed="1"
           >what do I do?</h2>
           <div className='projects-container'  >
             {projects.map(function (i, a) {
               return (
                 <div className='project' key={a} style={a % 2 ? { flexDirection: "row-reverse" } : { flexDirection: "row" }} >
-                  <h1 style={a % 2 ? { minWidth: "200px" } : { minWidth: "100px" }}
+                  <h1
+                  data-scroll
+                  data-scroll-speed="3"
+                  data-scroll-direction="vectical"
+
+  
+                  style={a % 2 ? { minWidth: "200px" } : { minWidth: "100px" }}
                   >{[i]}</h1>
-                  <p style={a % 2 ? { marginRight: "300px" } : { marginLeft: "300px", textAlign: "right" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse </p>
-                </div>)
+                  <p data-scroll
+                  data-scroll-speed="2"
+                  data-scroll-direction="vectical"
+
+                  
+
+                 style={a % 2 ? { marginRight: "300px" } : { marginLeft: "300px", textAlign: "right" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse </p>
+                </div>) 
             })}
           </div>
-
+            
         </div>
+        <div className='social-media-container'>
+              <span className='instagram'></span>
+              <span className='twitter'></span>
+              <span className='github'></span>
+              <span className='mail'></span>
+
+            </div>
       </div>
     </>
   );
