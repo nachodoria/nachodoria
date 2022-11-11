ec6ab0774564a1cbe3eecdf6a286d8e34b4e0802
Ignacio Doria <88548167+nachodoria@users.noreply.github.com>
2022-11-11 15:19:50 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import "./locomotive-scroll.css"
 import React, { useState, useEffect, useRef } from 'react';
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import LocomotiveScroll from 'locomotive-scroll';
 
 
@@ -9,7 +10,7 @@ function App() {
   const scrollRef = useRef();
 
 
-
+  AOS.init();
 
   useEffect(() => {
 
@@ -18,7 +19,7 @@ function App() {
       smooth: true,
     });
 
-    
+
 
   });
 
@@ -39,9 +40,9 @@ function App() {
 
   return (
     <>
-      <div className='scroll'  ref={scrollRef}>
+      <div className='scroll' ref={scrollRef}>
         <div className="main" data-scroll-container >
-          <div className="start" data-scroll-section data-scroll-section-id="section1" id='js-target'>
+          <div className="start" data-scroll-section data-scroll-section-id="section1" >
             <h1
               data-scroll
               data-scroll-speed="4"
@@ -71,30 +72,31 @@ function App() {
             data-scroll-position="top"
             data-scroll-speed="2">about me</h1>
           <h2
-           data-scroll
-           data-scroll-direction="vectical"
-           data-scroll-position="top"
-           data-scroll-delay	="0.1"
+            data-scroll
+            data-scroll-direction="vectical"
+            data-scroll-position="top"
+            data-scroll-delay="0.1"
 
-           data-scroll-speed="1">i'm an IT Student</h2>
+            data-scroll-speed="1">i'm an IT Student</h2>
           <p
-          data-scroll
-          data-scroll-speed="2"
-          data-scroll-direction="vectical"
-          
-           >My name is Ignacio Doria, I am a high school student in Argentina. Fortunately I discovered the front-end development skills and started developing them at a young age. I started to learn these skills in 2020 and now I can tell that I know JS, React JS, CSS, HTML, Python, and some PHP for some back-end needs. I am open-minded to the process of learning.</p>
+            data-scroll
+            data-scroll-speed="2"
+            data-scroll-direction="vectical"
+
+          >My name is Ignacio Doria, I am a high school student in Argentina. Fortunately I discovered the front-end development skills and started developing them at a young age. I started to learn these skills in 2020 and now I can tell that I know JS, React JS, CSS, HTML, Python, and some PHP for some back-end needs. I am open-minded to the process of learning.</p>
         </div>
 
         <div className='projects-section' data-scroll-section data-scroll-section-id="section3" >
           <h1 data-scroll
-            data-scroll-direction="vertical"
-            data-scroll-speed="1"
-            >projects</h1>
+            data-scroll-direction="vectical"
+            data-scroll-position="top"
+            data-scroll-speed="2"
+          >projects</h1>
           <h2
             data-scroll
             data-scroll-direction="vectical"
-            data-scroll-position="bottom" 
-             data-scroll-delay	="0.1"
+            data-scroll-position="top"
+            data-scroll-delay="0.1"
             data-scroll-speed="1"
           >what do I do?</h2>
           <div className='projects-container'  >
@@ -102,52 +104,54 @@ function App() {
               return (
                 <div className='project' key={a} style={a % 2 ? { flexDirection: "row-reverse" } : { flexDirection: "row" }} >
                   <h1
-                  data-scroll
-                  data-scroll-speed="4"
-                  data-scroll-direction="vectical"
+                    data-scroll
+                    data-scroll-speed="4"
+                    data-scroll-direction="vectical"
 
-  
-                  style={a % 2 ? { minWidth: "200px" } : { minWidth: "100px" }}
+
+                    style={a % 2 ? { minWidth: "200px" } : { minWidth: "100px" }}
                   >{[i]}</h1>
                   <p data-scroll
-                  data-scroll-speed="2"
-                  data-scroll-direction="vectical"
+                    data-scroll-speed="1"
+                    data-scroll-direction="vectical"
+                    
 
-                  
 
-                 style={a % 2 ? { marginRight: "300px" } : { marginLeft: "300px", textAlign: "right" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse </p>
-                </div>) 
+
+                    style={a % 2 ? { marginRight: "300px" } : { marginLeft: "300px", textAlign: "right" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse </p>
+                </div>)
             })}
           </div>
-          <div className='social-media-container' >
-              <svg 
-                data-scroll
-                data-scroll-speed="4"
-                data-scroll-direction="vectical"  
-                className='instagram'
-                rel="noreferrer" target="_blank" href='https://www.instagram.com/nacho.doria/'></svg>
-              <svg  
-              data-scroll
-              data-scroll-speed="3"
-              data-scroll-direction="vectical"
-              className='twitter'
-              rel="noreferrer" target="_blank" href='https://twitter.com/NachoDoria_'></svg>
-              <svg 
-              data-scroll
-              data-scroll-speed="2"
-              data-scroll-direction="vectical"
-              className='github'
-              rel="noreferrer" target="_blank" href='https://github.com/nachodoria'></svg>
-              <svg
-              data-scroll
-              data-scroll-speed="1"
-              data-scroll-direction="vectical"
-              onClick={() => window.location = 'mailto:ignaciodoriaoberman@gmail.com'}
-              className='mail'></svg>
-
-            </div>
         </div>
-        
+        <div className='social-media-container' data-scroll-section data-scroll-section-id="section4" >
+          <svg
+            data-scroll
+            data-scroll-speed="4"
+            data-scroll-direction="vectical"
+            className='instagram'
+            rel="noreferrer" target="_blank" href='https://www.instagram.com/nacho.doria/'></svg>
+          <svg
+            data-scroll
+            data-scroll-speed="3"
+            data-scroll-direction="vectical"
+            className='twitter'
+            rel="noreferrer" target="_blank" href='https://twitter.com/NachoDoria_'></svg>
+          <svg
+            data-scroll
+            data-scroll-speed="2"
+            data-scroll-direction="vectical"
+            className='github'
+            rel="noreferrer" target="_blank" href='https://github.com/nachodoria'></svg>
+          <svg
+            data-scroll
+            data-scroll-speed="1"
+            data-scroll-direction="vectical"
+            onClick={() => window.location = 'mailto:ignaciodoriaoberman@gmail.com'}
+            className='mail'></svg>
+
+        </div>
+
+
       </div>
     </>
   );
